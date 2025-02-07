88939bd5cc2d7d720ba1c716ea88dfd7b5156402
Matheus Luís <matheus.bernardi@lnls.br>
2025-02-07 17:02:44 -0300

    Add constants module and refactor main logic to use imported constants

--- main.ts
+++ main.ts
@@ -1,60 +1,9 @@
-const PAYLOAD_MAX_SIZE = 16368; // 16KB or 16368 bytes
-const HEADER_SIZE = 16; // 16 bytes
-const MAX_MESSAGE_SIZE = PAYLOAD_MAX_SIZE + HEADER_SIZE;
-const RESPONSE_SIZE = 1024;
-
-const commands = {
-  VERSION: 0,
-  READ: 3,
-  ECHO: 23,
-};
-
-const DBR_TYPES = {
-  DBR_STRING: 0,
-  DBR_INT: 1,
-  DBR_SHORT: 1,
-  DBR_FLOAT: 2,
-  DBR_ENUM: 3,
-  DBR_CHAR: 4,
-  DBR_LONG: 5,
-  DBR_DOUBLE: 6,
-  DBR_STS_STRING: 7,
-  DBR_STS_INT: 8,
-  DBR_STS_SHORT: 8,
-  DBR_STS_FLOAT: 9,
-  DBR_STS_ENUM: 10,
-  DBR_STS_CHAR: 11,
-  DBR_STS_LONG: 12,
-  DBR_STS_DOUBLE: 13,
-  DBR_TIME_STRING: 14,
-  DBR_TIME_INT: 15,
-  DBR_TIME_SHORT: 15,
-  DBR_TIME_FLOAT: 16,
-  DBR_TIME_ENUM: 17,
-  DBR_TIME_CHAR: 18,
-  DBR_TIME_LONG: 19,
-  DBR_TIME_DOUBLE: 20,
-  DBR_GR_STRING: 21,
-  DBR_GR_INT: 22,
-  DBR_GR_SHORT: 22,
-  DBR_GR_FLOAT: 23,
-  DBR_GR_ENUM: 24,
-  DBR_GR_CHAR: 25,
-  DBR_GR_LONG: 26,
-  DBR_GR_DOUBLE: 27,
-  DBR_CTRL_STRING: 28,
-  DBR_CTRL_INT: 29,
-  DBR_CTRL_SHORT: 29,
-  DBR_CTRL_FLOAT: 30,
-  DBR_CTRL_ENUM: 31,
-  DBR_CTRL_CHAR: 32,
-  DBR_CTRL_LONG: 33,
-  DBR_CTRL_DOUBLE: 34,
-  DBR_PUT_ACKT: 35,
-  DBR_PUT_ACKS: 36,
-  DBR_STSACK_STRING: 37,
-  DBR_CLASS_NAME: 38,
-};
+import {
+  commands,
+  DEFAULT_PRIORITY,
+  DEFAULT_VERSION,
+  RESPONSE_SIZE,
+} from "./constants.ts";
 
 type Header = {
   command: number; // UINT16 - 2 bytes
@@ -108,27 +57,47 @@ function requestVersion(priority: number, version: number): Uint8Array {
   return headerToBuffer(header);
 }
 
-async function main() {
+async function handshake(
+  conn: Deno.Conn,
+  priority: number = DEFAULT_PRIORITY,
+  version: number = DEFAULT_VERSION
+) {
+  const req = requestVersion(priority, version);
+  await conn.write(req);
+
+  const response = new Uint8Array(RESPONSE_SIZE);
+
+  await conn.read(response);
+  return headerFromBuffer(response);
+}
+
+async function createVirtualCircuit(hostname: string, port: number) {
   let conn;
   try {
     conn = await Deno.connect({
       transport: "tcp",
-      port: 5064,
-      hostname: "0.0.0.0",
+      port: port,
+      hostname: hostname,
     });
   } catch (err) {
     console.log(err);
     Deno.exit(1);
   }
-  const req = requestVersion(0, 0);
 
-  await conn.write(req);
+  const res = await handshake(conn);
 
-  const response = new Uint8Array(RESPONSE_SIZE);
-  await conn.read(response);
+  if (res.command !== commands.VERSION) {
+    console.log("Invalid response");
+    Deno.exit(1);
+  }
 
-  console.log(response);
-  console.log(headerFromBuffer(response));
+  return { conn };
+}
+
+async function main() {
+  const { conn } = await createVirtualCircuit("localhost", 5064);
+
+  console.log("Connected to server");
 }
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
