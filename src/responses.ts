5fe146724b506d8d4d57acd47cceb6921acc651c
Matheus Luís <matheus.bernardi@lnls.br>
2025-03-14 16:36:50 -0300

    feat: add parseData function for handling various data types from buffer

--- src/responses.ts
+++ src/responses.ts
@@ -1,5 +1,5 @@
 import type { Header, Response } from "./types.ts";
-import { commands, errors, HEADER_SIZE } from "./constants.ts";
+import { commands, DBR_TYPES, errors, HEADER_SIZE } from "./constants.ts";
 import { headerFromBuffer } from "./headers.ts";
 
 function payloadFromBuffer(
@@ -53,9 +53,76 @@ function decodeReadNotifyResponse(buf: Uint8Array): Response {
   return res;
 }
 
+type Data = number | string | boolean;
+
+function parseData(buf: Uint8Array, type: number, count: number): Data[] {
+  const view = new DataView(buf.buffer);
+
+  switch (type) {
+    case DBR_TYPES.INT: {
+      const intData: number[] = [];
+      for (let i = 0; i < count; i++) {
+        intData.push(view.getInt32(i * 4));
+      }
+      return intData;
+    }
+    case DBR_TYPES.STRING: {
+      const strData: string[] = [];
+      for (let i = 0; i < count; i++) {
+        const strLen = view.getInt32(i * 4);
+        const str = new TextDecoder().decode(
+          buf.slice(4 + i * 4, 4 + i * 4 + strLen),
+        );
+        strData.push(str);
+      }
+      return strData;
+    }
+    case DBR_TYPES.ENUM: {
+      const enumData: string[] = [];
+      for (let i = 0; i < count; i++) {
+        const enumVal = view.getInt16(i * 2);
+        enumData.push(enumVal.toString());
+      }
+      return enumData;
+    }
+    case DBR_TYPES.CHAR: {
+      const charData: string[] = [];
+      for (let i = 0; i < count; i++) {
+        const char = view.getInt8(i);
+        charData.push(String.fromCharCode(char));
+      }
+      return charData;
+    }
+    case DBR_TYPES.LONG: {
+      const longData: number[] = [];
+      for (let i = 0; i < count; i++) {
+        longData.push(view.getInt32(i * 4));
+      }
+      return longData;
+    }
+    case DBR_TYPES.DOUBLE: {
+      const doubleData: number[] = [];
+      for (let i = 0; i < count; i++) {
+        doubleData.push(view.getFloat64(i * 8));
+      }
+      return doubleData;
+    }
+    case DBR_TYPES.FLOAT: {
+      const floatData: number[] = [];
+      for (let i = 0; i < count; i++) {
+        floatData.push(view.getFloat32(i * 4));
+      }
+      return floatData;
+    }
+    default:
+      throw new Error("Unsupported data type");
+  }
+}
+
 export {
   decodeAccessRightsResponse,
   decodeCreateChannelResponse,
   decodeReadNotifyResponse,
   decodeSearchResponse,
+  parseData,
 };
